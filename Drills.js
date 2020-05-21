b265577dcc5b3d69457a74ac3ebfa6283d915a44
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-21 13:12:15 -0400

    completed maze problems

--- Drills.js
+++ Drills.js
@@ -95,65 +95,70 @@ const factorial = function(num) {
 //let testNum = 5;
 //console.log(factorial(testNum));
 
-/*
-8. Find a way out of the maze
-You have entered a Maze and need to find your way out of it. 
-There are more than one possible paths through the Maze to the single exit point. 
-Write a recursive function that will help you find a possible path though the maze.
+/*8. Find a way out of the maze & 9. Find ALL ways out of the maze*/
 
-You can use the following mazes to test your program.
+function escape(maze, str = '') {
+  const rs = str.split('').filter(c => c === 'R').length;
+  const ls = str.split('').filter(c => c === 'L').length;
+  const us = str.split('').filter(c => c === 'U').length;
+  const ds = str.split('').filter(c => c === 'D').length;
+  const x = rs - ls;
+  const y = ds - us;
 
-let mySmallMaze = [
-    [' ', ' ', ' '],
-    [' ', '*', ' '],
-    [' ', ' ', 'e']
+  let up;
+  let right;
+  let down;
+  let left;
+  if (maze[y - 1]) up = maze[y - 1][x];
+  right = maze[y][x + 1];
+  if (maze[y + 1]) down = maze[y + 1][x];
+  left = maze[y][x - 1];
+
+  //base
+  if ( // if no possible moves
+    (up === undefined || up === '*') &&
+    (right === undefined || right === '*') &&
+    (down === undefined || down === '*') &&
+    (left === undefined || left === '*')
+  ) { return; }
+  // if exit is found;
+  if (up === 'e') console.log(str + 'U');
+  if (right === 'e') console.log(str + 'R');
+  if (down === 'e') console.log(str + 'D');
+  if (left === 'e') console.log(str + 'L');
+
+  //recursive
+  if (up && up !== '*') {
+    const newMaze = [...maze].map(line => [...line]);
+    newMaze[y][x] = '*';
+    escape(newMaze, str + 'U');
+  }
+  if (right && right !== '*') {
+    const newMaze = [...maze].map(line => [...line]);
+    newMaze[y][x] = '*';
+    escape(newMaze, str + 'R');
+  }
+  if (down && down !== '*') {
+    const newMaze = [...maze].map(line => [...line]);
+    newMaze[y][x] = '*';
+    escape(newMaze, str + 'D');
+  }
+  if (left && left !== '*') {
+    const newMaze = [...maze].map(line => [...line]);
+    newMaze[y][x] = '*';
+    escape(newMaze, str + 'L');
+  }
+}
+
+let myVerySmallMaze = [
+  []
 ];
-
-let maze = [
-    [' ', ' ', ' ', '*', ' ', ' ', ' '],
-    ['*', '*', ' ', '*', ' ', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', '*', '*', '*', '*', '*', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+let enterIsExit = [
+  ['e']
 ];
-The Maze is represented as a N*M matrix (in the above case, a 3X3 or a 5X7 array). 
-The starting point is the top left corner and the exit is indicated by e. 
-For simplicity purpose, use the bottom right corner of the maze as the exit. 
-You can't go outside the boundaries of the maze. 
-The maze has passages that are blocked and you can't go through them. 
-These blocked passages are indicated by *. 
-Passing through a blocked cell as well as passing though a cell that you have already passed before are forbidden.
-
-For the above maze, a possible exit path can be RRDDLLDDRRRRRR
-*/
-const navigator = function(maze) {
-  let route = '';
-  const path = function(column, row) {
-    if(maze[column][row] === 'e') {
-      return route;
-    } else if(maze[column][row] === ' ') {
-      maze[column][row] = '*';
-      if(column < maze.length - 1) {
-        route.concat('D');
-        path(column + 1, row);
-      }
-      if(row < maze[column].length - 1) {
-        route.concat('R');
-        path(column, row + 1);
-      }
-      if(column === '*') {
-        route.concat('U');
-        path(column - 1, row);
-      }
-      if(row === '*') {
-        route.concat('L');
-        path(column, row -1);
-      }
-    }
-  };
-  return path(maze[0], maze[0][0]);
-};
-
+let justWalkRight = [ 
+  [' ', 'e'] 
+];
 let mySmallMaze = [
   [' ', ' ', ' '],
   [' ', '*', ' '],
@@ -167,22 +172,10 @@ let myMaze = [
   [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
-//console.log(navigator(mySmallMaze));
+//console.log(navigator(myVerySmallMaze));
+//console.log(navigator(enterIsExit));
+console.log(escape(myMaze));
 
-/*
-9. Find ALL the ways out of the maze
-Use the above maze and modify your solution so it finds All the possible exit paths through the Maze. 
-To find all possible exit paths through the maze, think about how many places you can move at each turn.
-Possibly up, down, left or right?
-
-Notice that this maze has 3 exits paths. 
-Your recursive function should print all three of the paths with the proper directions. 
-For example, given the maze above, the program should output the following:
-
-Path to the exit: RRDDLLDDRRRRRR
-Path to the exit: RRDDRRUURRDDDD
-Path to the exit: RRDDRRRRDD
-*/
 
 /*10. Anagrams*/
 const makeAnagrams = function(subject) {
